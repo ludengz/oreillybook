dfceb0db03ed0c08ac164dbf736f4b91bda9e17a
Ludeng Zhao <ludeng.zhao@gmail.com>
2026-03-15 00:47:26 -0700

    feat: implement e-ink optimizer with CSS injection and path rewriting

--- oreilly-epub-extension/lib/eink-optimizer.js
+++ oreilly-epub-extension/lib/eink-optimizer.js
@@ -1,1 +1,31 @@
-const EinkOptimizer = {};
+const EinkOptimizer = {
+  injectOverrideCss(xhtml) {
+    const link = '<link rel="stylesheet" type="text/css" href="../Styles/eink-override.css"/>';
+    if (xhtml.includes('</head>')) {
+      return xhtml.replace('</head>', `${link}\n</head>`);
+    }
+    return xhtml;
+  },
+
+  rewriteImagePaths(xhtml, imageMap) {
+    let result = xhtml;
+    for (const [original, newName] of Object.entries(imageMap)) {
+      result = result.split(original).join(`../Images/${newName}`);
+    }
+    return result;
+  },
+
+  rewriteCssLinks(xhtml) {
+    return xhtml.replace(
+      /href="([^"]*\.css)"/g,
+      (_, path) => `href="../Styles/${path.split('/').pop()}"`
+    );
+  },
+
+  processChapter(xhtml, imageMap) {
+    let result = this.rewriteCssLinks(xhtml);
+    result = this.rewriteImagePaths(result, imageMap);
+    result = this.injectOverrideCss(result);
+    return result;
+  },
+};
--- oreilly-epub-extension/tests/eink-optimizer.test.js
+++ oreilly-epub-extension/tests/eink-optimizer.test.js
@@ -1,1 +1,29 @@
-// Tests added in Task 5
+describe('EinkOptimizer.injectOverrideCss', function() {
+  it('adds eink-override.css link before </head>', function() {
+    const xhtml = '<?xml version="1.0"?><html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" href="styles.css"/></head><body></body></html>';
+    const result = EinkOptimizer.injectOverrideCss(xhtml);
+    assertContains(result, 'eink-override.css');
+    assert(result.indexOf('styles.css') < result.indexOf('eink-override.css'),
+      'eink-override must come after existing stylesheets');
+  });
+  it('handles XHTML with no existing stylesheets', function() {
+    const xhtml = '<?xml version="1.0"?><html xmlns="http://www.w3.org/1999/xhtml"><head><title>T</title></head><body></body></html>';
+    assertContains(EinkOptimizer.injectOverrideCss(xhtml), 'eink-override.css');
+  });
+});
+
+describe('EinkOptimizer.rewriteImagePaths', function() {
+  it('rewrites relative paths to EPUB Images/ dir', function() {
+    const xhtml = '<html><body><img src="../graphics/fig1.png"/></body></html>';
+    const result = EinkOptimizer.rewriteImagePaths(xhtml, { '../graphics/fig1.png': 'fig1.png' });
+    assertContains(result, 'src="../Images/fig1.png"');
+  });
+});
+
+describe('EinkOptimizer.rewriteCssLinks', function() {
+  it('rewrites CSS href to ../Styles/', function() {
+    const xhtml = '<html><head><link rel="stylesheet" href="css/book.css"/></head><body></body></html>';
+    const result = EinkOptimizer.rewriteCssLinks(xhtml);
+    assertContains(result, 'href="../Styles/book.css"');
+  });
+});
